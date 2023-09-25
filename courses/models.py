67939c2d245dce799c347fe4031812827eb11ec9
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-25 18:41:39 +0500

    Ch-12-Creating the Content models

--- courses/models.py
+++ courses/models.py
@@ -4,6 +4,37 @@ from django.contrib.contenttypes.models import ContentType
 from django.contrib.contenttypes.fields import GenericForeignKey
 
 
+class ItemBase(models.Model):
+    owner = models.ForeignKey(
+        User, related_name="%(class)s_related", on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+
+
+class Text(ItemBase):
+    content = models.TextField()
+
+
+class File(ItemBase):
+    content = models.FileField(upload_to="files")
+
+
+class Image(ItemBase):
+    file = models.FileField(upload_to="images")
+
+
+class Video(ItemBase):
+    url = models.URLField()
+
+
 class Subject(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200, unique=True)
@@ -45,7 +76,10 @@ class Module(models.Model):
 
 class Content(models.Model):
     module = models.ForeignKey(
-        Module, related_name="contents", on_delete=models.CASCADE
+        Module,
+        related_name="contents",
+        on_delete=models.CASCADE,
+        limit_choices_to={"model__in": ("text", "video", "image", "file")},
     )
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
