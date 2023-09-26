3adde21bd0798727e9472331ed85370d82be39f2
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-27 03:29:10 +0500

    Ch-14-Creating a student registration view

--- educa/settings.py
+++ educa/settings.py
@@ -36,13 +36,13 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "courses.apps.CoursesConfig",
-    "students.apps.StudentsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "students.apps.StudentsConfig",
 ]
 
 MIDDLEWARE = [
