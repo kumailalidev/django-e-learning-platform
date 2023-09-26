0a2dc5981fd8855d06a7b686fbffec857dbaaab8
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-27 01:40:23 +0500

    Ch-14-Adding student registration

--- educa/settings.py
+++ educa/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "courses.apps.CoursesConfig",
+    "students.apps.StudentsConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
