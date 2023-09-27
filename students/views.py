b3ab2f83477e7d31fcdcda8b7fc0d7eb84c58dfa
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-28 00:27:13 +0500

    Fixed logical error inside StudentCourseListView view; mistyped get_queryset function as get_query

--- students/views.py
+++ students/views.py
@@ -42,7 +42,7 @@ class StudentCourseListView(LoginRequiredMixin, ListView):
     model = Course
     template_name = "students/course/list.html"
 
-    def get_query(self):
+    def get_queryset(self):
         qs = super().get_queryset()
         return qs.filter(students__in=[self.request.user])
 
