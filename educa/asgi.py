1cca1a965f71300ff8604fe1a8d2b570ba687250
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-29 01:49:16 +0500

    Ch-16-Routing

--- educa/asgi.py
+++ educa/asgi.py
@@ -10,7 +10,9 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import chat.routing
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
 
@@ -19,5 +21,6 @@ django_asgi_app = get_asgi_application()
 application = ProtocolTypeRouter(
     {
         "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(URLRouter(chat.routing.websocket_urlpatterns)),
     }
 )
