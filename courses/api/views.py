f362b2c1312ffb56f0122a5fb3accd5d9ed23204
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-28 05:16:44 +0500

    Ch-15-Handling authentication

--- courses/api/views.py
+++ courses/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
+from rest_framework.authentication import BasicAuthentication
 from courses.models import Subject, Course
 from courses.api.serializers import SubjectSerializer
 
@@ -17,6 +18,8 @@ class SubjectDetailView(generics.RetrieveAPIView):
 
 
 class CourseEnrollView(APIView):
+    authentication_classes = [BasicAuthentication]
+
     def post(self, request, pk, format=None):
         course = get_object_or_404(Course, pk=pk)
         course.students.add(request.user)
