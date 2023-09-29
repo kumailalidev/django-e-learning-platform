279cfbaa2d7e29a444053b39504a5a39241627fa
Kumail Ali Shaikh <kumailalidev@gmail.com>
2023-09-30 03:26:48 +0500

    Ch-16-Setting up a channel layer with Redis and Updating consumer to boradcast message

--- educa/settings.py
+++ educa/settings.py
@@ -146,3 +146,12 @@ REST_FRAMEWORK = {
 }
 
 ASGI_APPLICATION = "educa.asgi.application"
+
+CHANNEL_LAYERS = {
+    "default": {
+        "BACKEND": "channels_redis.core.RedisChannelLayer",
+        "CONFIG": {
+            "hosts": [("127.0.0.1", 6379)],
+        },
+    },
+}
