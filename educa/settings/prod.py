b222ff1bcf79ebdc0d69f6b0375d8ba6ff90579b
kumailalidev <kumailalidev@gmail.com>
2023-11-28 21:20:38 +0500

    Ch-17-Production environemnt settings.

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -1,1 +1,13 @@
 # Custom settings for production environment
+
+from .base import *
+
+DEBUG = False
+
+ADMINS = [
+    ("Django Admin", "email@mydomain.com"),
+]
+
+ALLOWED_HOSTS = ["*"]
+
+DATABASES = {"default": {}}
