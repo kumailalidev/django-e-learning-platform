f36501b05c36344223a2b96a673b3c0e3ad8f778
kumailalidev <kumailalidev@gmail.com>
2023-11-28 21:01:32 +0500

    Ch-17-Managing settings for multiple environment.

--- educa/settings/base.py
+++ educa/settings/base.py
@@ -18,7 +18,8 @@ from django.urls import reverse_lazy
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+print(BASE_DIR)
 
 
 # Quick-start development settings - unsuitable for production
