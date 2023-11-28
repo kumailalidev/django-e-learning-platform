8ee9de78e8093685774206fdcfbdf512232baf8e
kumailalidev <kumailalidev@gmail.com>
2023-11-28 23:04:48 +0500

    Ch-17-Configuring the PostgreSQL service.

--- educa/settings/prod.py
+++ educa/settings/prod.py
@@ -10,4 +10,13 @@ ADMINS = [
 
 ALLOWED_HOSTS = ["*"]
 
-DATABASES = {"default": {}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": "db",
+        "PORT": 5432,
+    }
+}
