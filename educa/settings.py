ab994f63e16661d71a820c26f3d43b826ff0dae4
kumailalidev <kumailalidev@gmail.com>
2023-11-28 19:20:32 +0500

    Ch-14-Using the pre-site cache.

--- educa/settings.py
+++ educa/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    # "django.middleware.cache.UpdateCacheMiddleware",
     "django.middleware.common.CommonMiddleware",
+    # "django.middleware.cache.FetchFromCacheMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -147,6 +149,10 @@ CACHES = {
         "LOCATION": "127.0.0.1:11211",
     }
 }
+
+CACHE_MIDDLEWARE_ALIAS = "default"
+CACHE_MIDDLEWARE_SECONDS = 60 * 15  # 15 minutes
+CACHE_MIDDLEWARE_KEY_PREFIX = "educa"
 
 INTERNAL_IPS = [
     "127.0.0.1",
