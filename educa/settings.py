f3d04cdb5d61d8ad461117521bc355a089e43432
kumailalidev <kumailalidev@gmail.com>
2023-11-28 18:19:42 +0500

    [FIX]Ch-14-Adding Memcached to your project

--- educa/settings.py
+++ educa/settings.py
@@ -141,7 +141,7 @@ LOGIN_REDIRECT_URL = reverse_lazy("student_course_list")
 
 CACHES = {
     "default": {
-        "BACKED": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
         "LOCATION": "127.0.0.1:11211",
     }
 }
